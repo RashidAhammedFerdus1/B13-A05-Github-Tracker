a48509ba35002765c157b081e15a3c088a08f6d0
RashidAhammedFerdus1 <raferdus53@gmail.com>
2026-03-09 21:55:23 +0600

    button click

--- js/api.js
+++ js/api.js
@@ -1,56 +1,56 @@
 let issues = [];
 
 async function issuesFetchData() {
-  const res = await fetch(
-    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
-  );
-  const data = await res.json();
-  issues = data.data;
+    const res = await fetch(
+        "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+    );
+    const data = await res.json();
+    issues = data.data;
 
-  displayIssues(issues);
+    displayIssues(issues);
 }
 // display issues
 
 const displayIssues = (issues) => {
-  //   console.log(issues);
-  const issuesContainer = document.getElementById("container");
-  issuesContainer.innerHTML = "";
-  issues.forEach((item) => {
-    let statusIcons = "";
-    let priorityColor = "";
-    // console.log(item);
+    //   console.log(issues);
+    const issuesContainer = document.getElementById("container");
+    issuesContainer.innerHTML = "";
+    issues.forEach((item) => {
+        let statusIcons = "";
+        let priorityColor = "";
+        // console.log(item);
 
-    const issuesAppended = document.createElement("div");
+        const issuesAppended = document.createElement("div");
 
-    issuesAppended.className =
-      "bg-white p-5 space-y-4 border-t-gray-500 border-t-5 rounded-xl";
-    // for open and closed border
-    if (item.status === "open") {
-      //   console.log(item.status);
-      issuesAppended.classList.replace(
-        "border-t-gray-500",
-        "border-t-green-500",
-      );
-      statusIcons = "assets/Open-Status.png";
-    } else if (item.status === "closed") {
-      issuesAppended.classList.replace(
-        "border-t-gray-500",
-        "border-t-purple-500",
-      );
-      statusIcons = "assets/Closed- Status .png";
-    }
+        issuesAppended.className =
+            "bg-white p-5 space-y-4 border-t-gray-500 border-t-5 rounded-xl";
+        // for open and closed border
+        if (item.status === "open") {
+            //   console.log(item.status);
+            issuesAppended.classList.replace(
+                "border-t-gray-500",
+                "border-t-green-500",
+            );
+            statusIcons = "assets/Open-Status.png";
+        } else if (item.status === "closed") {
+            issuesAppended.classList.replace(
+                "border-t-gray-500",
+                "border-t-purple-500",
+            );
+            statusIcons = "assets/Closed- Status .png";
+        }
 
-    if (item.priority === "high") {
-      priorityColor = "bg-[#FEECEC] text-[#EF4444]";
-    } else if (item.priority === "medium") {
-      priorityColor = "bg-[#FFF6D1] text-[#F59E0B]";
-    } else if (item.priority === "low") {
-      priorityColor = "bg-gray-300 text-gray-600";
-    } else {
-      priorityColor = "bg-gray-300 text-black";
-    }
+        if (item.priority === "high") {
+            priorityColor = "bg-[#FEECEC] text-[#EF4444]";
+        } else if (item.priority === "medium") {
+            priorityColor = "bg-[#FFF6D1] text-[#F59E0B]";
+        } else if (item.priority === "low") {
+            priorityColor = "bg-gray-300 text-gray-600";
+        } else {
+            priorityColor = "bg-gray-300 text-black";
+        }
 
-    issuesAppended.innerHTML = `
+        issuesAppended.innerHTML = `
     <div  class="flex items-center  justify-between space-x-2 ">
             <img src= "${statusIcons}" alt=""> 
             <p onclick="useModal(${item.id})" class="${priorityColor} rounded-3xl px-5 py-1">${item.priority}</p>
@@ -74,9 +74,9 @@ const displayIssues = (issues) => {
                         <P><p>${new Date(item.createdAt).toLocaleDateString()}</p></P>
                     </div>
     `;
-    issuesContainer.appendChild(issuesAppended);
-  });
-  // hiddenLoading();
+        issuesContainer.appendChild(issuesAppended);
+    });
+    // hiddenLoading();
 };
 
 const allbtn = document.getElementById("all-btn");
@@ -86,48 +86,74 @@ const countIssues = document.getElementById("countIssues");
 const loadingScan = document.getElementById("loadingScan");
 // showing
 function showLoading() {
-  loadingScan.classList.add("flex");
-  loadingScan.classList.remove("hidden");
+    loadingScan.classList.add("flex");
+    loadingScan.classList.remove("hidden");
 }
 // hidden
 function hiddenLoading() {
-  loadingScan.classList.add("hidden");
-  loadingScan.classList.remove("flex");
+    loadingScan.classList.add("hidden");
+    loadingScan.classList.remove("flex");
 }
 
 let allBtnStatus = "all-btn";
 function toggleBtn(id) {
-  allBtnStatus = id;
-  showLoading();
-  // remove color
-  allbtn.classList.remove("btn-primary", "text-white");
-  openBtn.classList.remove("btn-primary", "text-white");
-  closeBtn.classList.remove("btn-primary", "text-white");
-  // add color
-  allbtn.classList.add("bg-white", "text-black");
-  openBtn.classList.add("bg-white", "text-black");
-  closeBtn.classList.add("bg-white", "text-black");
+    allBtnStatus = id;
+    showLoading();
+    // remove color
+    allbtn.classList.remove("btn-primary", "text-white");
+    openBtn.classList.remove("btn-primary", "text-white");
+    closeBtn.classList.remove("btn-primary", "text-white");
+    // add color
+    allbtn.classList.add("bg-white", "text-black");
+    openBtn.classList.add("bg-white", "text-black");
+    closeBtn.classList.add("bg-white", "text-black");
 
-  const clickbtn = document.getElementById(id);
-  clickbtn.classList.add("bg-blue-500", "text-white");
-  clickbtn.classList.remove("bg-white", "text-black");
-  // section
+    const clickbtn = document.getElementById(id);
+    clickbtn.classList.add("bg-blue-500", "text-white");
+    clickbtn.classList.remove("bg-white", "text-black");
+    // section
 
-  setTimeout(() => {
-    if (id === "open-btn") {
-      filteredIssues = issues.filter((issue) => issue.status === "open");
-      displayIssues(filteredIssues);
+    setTimeout(() => {
+        if (id === "open-btn") {
+            filteredIssues = issues.filter((issue) => issue.status === "open");
+            displayIssues(filteredIssues);
 
-      document.getElementById("countIssues").innerText = filteredIssues.length;
-    } else if (id === "close-btn") {
-      filteredIssues = issues.filter((issue) => issue.status === "closed");
-      displayIssues(filteredIssues);
-      // hiddenLoading();
-      document.getElementById("countIssues").innerText = filteredIssues.length;
-    } else if (id === "all-btn") {
-      displayIssues(issues);
-      document.getElementById("countIssues").innerText = issues.length;
-    }
-    hiddenLoading();
-  }, 300);
+            document.getElementById("countIssues").innerText = filteredIssues.length;
+        } else if (id === "close-btn") {
+            filteredIssues = issues.filter((issue) => issue.status === "closed");
+            displayIssues(filteredIssues);
+            // hiddenLoading();
+            document.getElementById("countIssues").innerText = filteredIssues.length;
+        } else if (id === "all-btn") {
+            displayIssues(issues);
+            document.getElementById("countIssues").innerText = issues.length;
+        }
+        hiddenLoading();
+    }, 300);
 }
+document.getElementById("search-btn")
+    .addEventListener("click", async function () {
+        const searchValue = document.getElementById("searchValue");
+
+        const ValueInput = searchValue.value.trim().toLowerCase();
+        if (ValueInput === "") {
+            alert("Enter Word");
+            return;
+        }
+        console.log(ValueInput);
+        const res = await fetch(
+            `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${ValueInput}`,
+        );
+        const data = await res.json();
+        let maindata = data.data;
+
+        console.log(data.data);
+
+        if (maindata.length === 0) {
+            alert("Sorry !! No word found");
+            return;
+        }
+        document.getElementById("countIssues").innerText = maindata.length;
+        displayIssues(maindata);
+        searchValue.value = "";
+    });
